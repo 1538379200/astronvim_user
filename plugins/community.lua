a13eaaf45c678cb7753ed3546044d91f7d4aa793
1538379200 <1538379200@qq.com>
2023-07-13 22:22:49 +0800

    add some plugin

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,7 +6,8 @@ return {
 
   { import = "astrocommunity.motion.flash-nvim" },
   { import = "astrocommunity.scrolling.neoscroll-nvim" },
-  { import = "astrocommunity.test.neotest" }
+  { import = "astrocommunity.test.neotest" },
+  { import = "astrocommunity.editing-support.multicursors-nvim" }
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
