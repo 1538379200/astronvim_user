5b89ec79c53ab1825a8688cf881462d3cad71f38
胡 佐政 <huzuozheng@noway.cn>
2023-07-17 11:45:11 +0800

    add function move

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -4,6 +4,19 @@ return {
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
       -- "lua"
+      "lua", "python", "go", "rust", "markdown", "yaml", "toml"
+    })
+    return require("astronvim.utils").extend_tbl(opts, {
+      textobjects = {
+        move = {
+          goto_next_start = {
+            ["<A-j>"] = { query = "@function.outer", desc = "Next function start" },
+          },
+          goto_previous_start = {
+            ["<A-k>"] = { query = "@function.outer", desc = "Previous function start" },
+          },
+        },
+      },
     })
   end,
 }
