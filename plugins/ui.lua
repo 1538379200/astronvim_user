07402957cd6dcc18fa056a1cafecfebf0f91461b
胡 佐政 <huzuozheng@noway.cn>
2023-07-28 09:55:27 +0800

    重新安装相关主题

--- plugins/ui.lua
+++ plugins/ui.lua
@@ -1,13 +1,47 @@
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    config = function()
-        require("catppuccin").setup {
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        opts = {
+            integrations = {
+                alpha = true,
+                aerial = true,
+                dap = { enabled = true, enable_ui = true },
+                mason = true,
+                neotree = true,
+                notify = true,
+                nvimtree = false,
+                semantic_tokens = true,
+                symbols_outline = true,
+                telescope = true,
+                ts_rainbow = false,
+                which_key = true,
+            },
             no_italic = true,
             styles = {
                 comments = {},
-                conditionals = {}
-            }
-        }
-    end
+                conditionals = {},
+                loops = {},
+                functions = {},
+                keywords = {},
+                strings = {},
+                variables = {},
+                numbers = {},
+                booleans = {},
+                properties = {},
+                types = {},
+                operators = {},
+            },
+        },
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        optional = true,
+        opts = {
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
+        },
+    },
 }
