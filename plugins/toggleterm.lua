d4697223b918f5cdb6f27c350ab9cb6adb3501cd
huzuozheng <huzuozheng@noway.cn>
2023-09-11 09:50:11 +0800

    修改 terminal 部分处理

--- plugins/toggleterm.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "akinsho/toggleterm.nvim",
-    opts = function(_, opts)
-        return require("astronvim.utils").extend_tbl(opts, {
-            -- open_mapping = [[<C-\>]],
-            start_in_insert = false
-        })
-    end
-}
