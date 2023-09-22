8bd47f0368e89f2bcd60f84182da6d6283cae9b2
huzuozheng <huzuozheng@noway.cn>
2023-09-22 15:16:40 +0800

    增加noice插件

--- plugins/noice.lua
+++ plugins/noice.lua
@@ -11,7 +11,8 @@ return {
     -- },
     {
         "folke/noice.nvim",
-        event = "VeryLazy",
+        -- event = "VeryLazy",
+        event = "VimEnter",
         -- cond = not vim.g.neovide,
         dependencies = { "MunifTanjim/nui.nvim" },
         opts = function(_, opts)
@@ -28,7 +29,7 @@ return {
                     },
                 },
                 presets = {
-                    bottom_search = true,                              -- use a classic bottom cmdline for search
+                    bottom_search = false,                              -- use a classic bottom cmdline for search
                     command_palette = true,                            -- position the cmdline and popupmenu together
                     long_message_to_split = true,                      -- long messages will be sent to a split
                     inc_rename = utils.is_available "inc-rename.nvim", -- enables an input dialog for inc-rename.nvim
