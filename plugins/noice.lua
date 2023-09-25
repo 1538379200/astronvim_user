2f977047ebbe9973cbff941421650cb0f77910cf
huzuozheng <huzuozheng@noway.cn>
2023-09-25 10:37:56 +0800

    修改telescope快捷键

--- plugins/noice.lua
+++ plugins/noice.lua
@@ -1,14 +1,5 @@
 local utils = require "astronvim.utils"
 return {
-    -- {
-    --   "nvim-treesitter/nvim-treesitter",
-    --   opts = function(_, opts)
-    --     if opts.ensure_installed ~= "all" then
-    --       opts.ensure_installed =
-    --         utils.list_insert_unique(opts.ensure_installed, { "bash", "markdown", "markdown_inline", "regex", "vim" })
-    --     end
-    --   end,
-    -- },
     {
         "folke/noice.nvim",
         -- event = "VeryLazy",
